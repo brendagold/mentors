52ddd8fdd63b8930aee52dc5bbb4f5635b55033b
brendagold <petersbrenda47@gmail.com>
2022-09-13 22:09:10 +0100

    changes made to hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,12 @@ import Image from "../images/mentors.png"
 
 const styles = {
     paperContainer: {
-    backgroundImage: `url(${Image})`
+    backgroundImage: `url(${Image})`,
+    height: "400px",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundColor: "#606060",
+    backgroundBlendMode: "multiply"
     }
 }
 
@@ -12,12 +17,13 @@ const Hero = () => {
   return (
     <div>
       <Paper elevation={0} style={styles.paperContainer}>
-        <Box sx={{ padding: "70px 0", flexGrow: 1, justifyContent: "center", alignItems: "center", display: "flex", flexDirection: "column" }}>
-          <Typography>Find a Professional Mentor right for you!</Typography>
-          <Typography>We have you covered</Typography>
+        <Box sx={{ paddingTop: "80px", flexGrow: 1, color: "white", justifyContent: "center", alignItems: "center", display: "flex", flexDirection: "column" }}>
+          <Typography variant="h4">Find a Professional Mentor right for you!</Typography>
+          <Typography variant="h6">We have you covered</Typography>
           <Button variant="contained">Get Started</Button>
         </Box>
       </Paper>
+      
     </div>
   );
 };
