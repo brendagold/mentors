57c37b66a5c1cb52ba3519795766c95d695b7217
brendagold <petersbrenda47@gmail.com>
2022-09-13 20:54:04 +0100

    added theme color

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { appTheme } from "./themes/theme";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={appTheme}>
+    <CssBaseline enableColorScheme />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
+    
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/themes/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+import { green, red } from "@mui/material/colors";
+
+export const appTheme = createTheme({
+  palette: {
+    primary: {
+      main: green[900],
+    },
+  },
+});
