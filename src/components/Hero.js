c591787def692afdaaaa13a4a2512901cd2775ff
brendagold <petersbrenda47@gmail.com>
2022-09-14 19:55:56 +0100

    added to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,29 +1,55 @@
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
-import Image from "../images/mentors.png"
+import Image from "../images/mentors.png";
 
 const styles = {
-    paperContainer: {
+  paperContainer: {
     backgroundImage: `url(${Image})`,
     height: "400px",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundColor: "#606060",
-    backgroundBlendMode: "multiply"
-    }
-}
+    backgroundBlendMode: "multiply",
+  },
+};
 
 const Hero = () => {
   return (
     <div>
       <Paper elevation={0} style={styles.paperContainer}>
-        <Box sx={{ paddingTop: "80px", flexGrow: 1, color: "white", justifyContent: "center", alignItems: "center", display: "flex", flexDirection: "column" }}>
-          <Typography variant="h4">Find a Professional Mentor right for you!</Typography>
+        <Box
+          sx={{
+            paddingTop: "80px",
+            flexGrow: 1,
+            color: "white",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Typography variant="h4">
+            Find a Professional Mentor right for you!
+          </Typography>
           <Typography variant="h6">We have you covered</Typography>
           <Button variant="contained">Get Started</Button>
         </Box>
       </Paper>
       
+      <Paper>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+         
+        </Grid>
+      </Paper>
     </div>
   );
 };
